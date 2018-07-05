41c48f786a32c6ea2ab8fe9b2bcc034436732d93
Prabhag0691 <40758244+Prabhag0691@users.noreply.github.com>
2018-07-05 19:50:15 +0530

    Update StartAppTest.java

--- src/test/java/StartAppTest.java
+++ src/test/java/StartAppTest.java
@@ -27,24 +27,32 @@ public class StartAppTest {
 	@Rule
 	public TestWatcher watcher = Factory.createWatcher();
 
-	private static EnhancedAndroidDriver<WebElement> driver;
-	private static  EnhancedAndroidDriver<WebElement> startApp() throws MalformedURLException  {
+	private static EnhancedIOSDriver<WebElement> driver;
+	private static  EnhancedIOSDriver<WebElement> startApp() throws MalformedURLException  {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 
-		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		/*capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1");																																														
 		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "03157df33ccbbd36");
 		capabilities.setCapability(MobileCapabilityType.APP, "/Users/Mobile/Desktop/Newjersy.apk");
-		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 7913);
+		capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 7913);*/
 	//	capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 
 		//	capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 7913);
 
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+		capabilities.setCapability("platformVersion", "10.3.3");
+		capabilities.setCapability("deviceName", "Iphone6 Black New");
+		capabilities.setCapability("udid", "d1c25a34a020a1e004f345be793a3ee5c459250c");
+		//desiredCapabilities.setCapability("automationName", "XCUITest");
+		capabilities.setCapability("app", "/Users/Mobile/Desktop/App Center/nj.ipa");
+
+		
 		URL url = new URL("http://localhost:4723/wd/hub");
 		//driver = (EnhancedAndroidDriver) new AndroidDriver(url, capabilities);
 
 
-		return Factory.createAndroidDriver(url, capabilities);
+		return Factory.createIOSDriver(url, capabilities);
 	}
 
 
